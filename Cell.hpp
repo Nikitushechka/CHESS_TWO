ecf46e79bb45877f2e52a2a0ae259a72d4e00031
VauReo <kushnir.ea@phystech.edu>
2022-12-12 20:48:13 +0300

    Partly made a Game.init() method, added getter for is_empty_ field

--- Cell.hpp
+++ Cell.hpp
@@ -14,6 +14,8 @@ class Cell{
 public:
     Cell(char hor_, char ver_): hor_(hor_), ver_(ver_), is_empty_(1), piece_(nullptr) {}
     Cell(char hor_, char ver_, Piece& piece_): hor_(hor_), ver_(ver_), is_empty_(0), piece_(make_unique<Piece>(piece_)) {}
+
+    bool IsEmpty() {return is_empty_;}
 };
 
 #endif // CELL_HPP_INCLUDED
