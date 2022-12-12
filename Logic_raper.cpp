0083983beb62ac89d50f8414f5b4a852ffd4f97f
VauReo <kushnir.ea@phystech.edu>
2022-12-12 20:48:13 +0300

    Partly made a Game.init() method, added getter for is_empty_ field

--- Logic_raper.cpp
+++ Logic_raper.cpp
@@ -11,13 +11,29 @@ Game::Game() {
 
 void Game::init() {
 
+    vector<vector<Cell>> default_board(8);
 
-    for (char i = 1; i < 9; ++i) {
-        for (char j = 1; j < 9; ++j) {
-            this->board_[i][j] = Cell(i, j);
+    default_board[0] = {Cell(1, 1, Rook(0)), Cell(1, 2, Knight(0)), Cell(1, 3, Bishop(0)), Cell(1, 4, Queen(0)),
+                        Cell(1, 5, King(0)), Cell(1, 6, Bishop(0)), Cell(1, 7, Knight(0)), Cell(1, 8, Rook(0) )};
+
+    for (int i = 1; i < 9; ++i) default_board[1].push_back(Cell(2, i, Pawn(0)));
+    for (int i = 2; i < 6; ++i) {
+        for (int j = 1; j < 9; ++j) {
+            default_board[i].push_back(Cell(i+1, j));
         }
     }
-}
+    for (int i = 1; i < 9; ++i) default_board[6].push_back(Cell(7, i, Pawn(1)));
+    default_board[7] = {Cell(8, 1, Rook(1)), Cell(8, 2, Knight(1)), Cell(8, 3, Bishop(1)), Cell(8, 4, Queen(1)),
+                        Cell(8, 5, King(1)), Cell(8, 6, Bishop(1)), Cell(8, 7, Knight(1)), Cell(8, 8, Rook(1) )};
+
+    this->board_ = default_board;
+    for (vector<Cell>& i: default_board) {
+        for (Cell& j: i) {
+            if (j.IsEmpty())
+        }
+    }
+
+    }
 
 int Game::start() {
     this->init();
